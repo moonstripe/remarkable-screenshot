5bcecd4bfc06e9109b6beccee266a49c778ced1f
moonstripe <moonstripe@protonmail.com>
2025-02-24 20:15:01 -0800

    non default behavior

--- main.ts
+++ main.ts
@@ -75,55 +75,56 @@ export default class MyPlugin extends Plugin {
 				// Called when the user clicks the icon.
 				// TODO: by default, screenshot. maybe open settings?
 				await this.callResnapRs();
-				new Notice("This is a notice!");
+				new Notice("Took screenshot");
 			},
 		);
 		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass("my-plugin-ribbon-class");
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
+		statusBarItemEl.setText("Screenshot");
 
 		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
+		// this.addCommand({
+		// 	id: "open-sample-modal-simple",
+		// 	name: "Open sample modal (simple)",
+		// 	callback: () => {
+		// 		new SampleModal(this.app).open();
+		// 	},
+		// });
+
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
+			id: "insert-remarkable-screenshot",
+			name: "Insert Remarkable Screeenshot",
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				console.log(await this.callResnapRs());
 				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
+				editor.replaceSelection("Took Screenshot");
 			},
 		});
 
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
+		// // This adds a complex command that can check whether the current state of the app allows execution of the command
+		// this.addCommand({
+		// 	id: "open-sample-modal-complex",
+		// 	name: "Open sample modal (complex)",
+		// 	checkCallback: (checking: boolean) => {
+		// 		// Conditions to check
+		// 		const markdownView =
+		// 			this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (markdownView) {
+		// 			// If checking is true, we're simply "checking" if the command can be run.
+		// 			// If checking is false, then we want to actually perform the operation.
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+		//
+		// 			// This command will only show up in Command Palette when the check function returns true
+		// 			return true;
+		// 		}
+		// 	},
+		// });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
@@ -185,6 +186,20 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		containerEl.createEl("h2", "Remarkable 2.0 Screenshot");
+
+		new Setting(containerEl)
+			.setName("Screenshot directory")
+			.setDesc(
+				"Specify a directory to put generated screenshots. Defaults to 'remarkable_screenshots'",
+			)
+			.addText((text) => {
+				text.setPlaceholder("Enter screenshot directory")
+					.setValue(this.plugin.settings.imagesDir)
+					.onChange(async (value) => {
+						this.plugin.settings.imagesDir = value;
+						await this.plugin.saveSettings();
+					});
+			});
 
 		new Setting(containerEl)
 			.setName("Remarkable IP")
