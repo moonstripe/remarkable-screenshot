ddc6b826b101f317d3819cdc363df5c3b043ec70
moonstripe <moonstripe@protonmail.com>
2025-02-24 22:33:48 -0800

    styles

--- main.ts
+++ main.ts
@@ -3,8 +3,6 @@ import {
 	Editor,
 	FileSystemAdapter,
 	MarkdownView,
-	Modal,
-	normalizePath,
 	Notice,
 	Plugin,
 	PluginSettingTab,
@@ -88,8 +86,8 @@ export default class MyPlugin extends Plugin {
 			async (evt: MouseEvent) => {
 				// Called when the user clicks the icon.
 				// TODO: by default, screenshot. maybe open settings?
-				await this.callResnapRs();
-				new Notice("Took screenshot");
+				let realPath = await this.callResnapRs();
+				new Notice(`Took screenshot: ${realPath}`);
 			},
 		);
 		// Perform additional things with the ribbon
@@ -98,15 +96,6 @@ export default class MyPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText("Screenshot");
-
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: "open-sample-modal-simple",
-		// 	name: "Open sample modal (simple)",
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	},
-		// });
 
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
@@ -123,40 +112,8 @@ export default class MyPlugin extends Plugin {
 			},
 		});
 
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: "open-sample-modal-complex",
-		// 	name: "Open sample modal (complex)",
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView =
-		// 			this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-		//
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	},
-		// });
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-		// 	console.log("click", evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(
-		// 	window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
-		// );
 	}
 
 	onunload() {}
@@ -173,22 +130,6 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-//
-// 	onOpen() {
-// 		const { contentEl } = this;
-// 		contentEl.setText("Woah!");
-// 	}
-//
-// 	onClose() {
-// 		const { contentEl } = this;
-// 		contentEl.empty();
-// 	}
-// }
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
